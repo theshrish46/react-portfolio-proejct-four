1ae53f05a4532db8e443654172137c6ffa1d9a12
theshrish46 <theshrish46@gmail.com>
2023-03-28 14:27:38 +0530

    on 28-3-23

---
+++ my-portfolio/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../src/styles/globals.css';
+
+const page = () => {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  )
+}
+
+export default page
