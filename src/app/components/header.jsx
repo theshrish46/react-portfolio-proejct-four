ea3207136dd54fa98b456f9d2da4a79588a05224
theshrish46 <theshrish46@gmail.com>
2023-04-21 13:24:00 +0530

    21-4-24

---
+++ src/app/components/header.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { BsFillSunFill } from 'react-icons/bs';
+import { FaTimes } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+
+
+// Style files
+import './../scss/global.scss';
+import './../scss/color.scss';
+import './../scss/header.scss';
+
+const header = () => {
+    return (
+        <div className='container'>
+            <header>
+
+
+                {/* Smaller Screens */}
+
+                <button className='button bars'><FaBars /></button>
+                <button className='button sun'><BsFillSunFill /></button>
+                <button className='button times'><FaTimes /></button>
+
+                <ul className='small__screen'>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#work">Work</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                    <li></li>
+                    <li>
+                        <a href="#">Resume</a>
+                    </li>
+                </ul>
+
+
+
+                <ul className='large-screen'>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#work">Work</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                    <li></li>
+                    <li>
+                        <a href="#">Resume</a>
+                    </li>
+
+                    <li>
+                        <a href="#"><BsFillSunFill /></a>
+                    </li>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+export default header
---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import { Inter } from 'next/font/google'
+
+import Header from './components/header';
+
+
+// Style Files
+import './scss/color.scss';
+import './scss/global.scss';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
